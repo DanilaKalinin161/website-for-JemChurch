bc3ef038ea1f01e0d13a9a5e8b068bfb57c1d2b1
DanilaKalinin161 <aooa32544@gmail.com>
2024-08-04 20:02:51 -0400

    finished setting up the language change create DB

--- main/views.py
+++ main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
+from main.models import Slider
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = Slider.objects.order_by('name')[:1]
+    slider_bd_two = Slider.objects.order_by('name')[:2]
+
+    return render(request, 'main/index.html', {'context': context, 'slider_bd_two': slider_bd_two})
 def about(request):
     return render(request, 'main/about.html')
 def donate(request):
