1e1535f48315a730ed5ecae0392b71a2e26eb66a
DanilaKalinin161 <aooa32544@gmail.com>
2024-08-04 20:02:51 -0400

    finished setting up the language change

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils.translation import gettext as _
 
 def index(request):
     return render(request, 'main/index.html')
